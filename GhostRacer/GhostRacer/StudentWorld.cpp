e00a57524860dedfeff7d7e6274a63f385614930
Liam Greubel <liam.greubel@gmail.com>
2021-02-20 02:51:16 -0800

    lag w solid lines

--- GhostRacer/GhostRacer/StudentWorld.cpp
+++ GhostRacer/GhostRacer/StudentWorld.cpp
@@ -24,23 +24,23 @@ int StudentWorld::init()
     int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH/2;
     int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH/2;
     int N = VIEW_HEIGHT / SPRITE_HEIGHT;
-    /*int M = VIEW_HEIGHT / (4 * SPRITE_HEIGHT);
+    int M = VIEW_HEIGHT / (4 * SPRITE_HEIGHT);
     int left_middle = LEFT_EDGE + (ROAD_WIDTH / 3);
-    int right_middle = RIGHT_EDGE - (ROAD_WIDTH / 3);*/
+    int right_middle = RIGHT_EDGE - (ROAD_WIDTH / 3);
     for(int i = 0; i < N; i++)
     {
         Yellow* leftLine = new Yellow(LEFT_EDGE,VIEW_HEIGHT-(i*SPRITE_HEIGHT),this,liam);
         m_vector.push_back(leftLine);
         Yellow* rightLine = new Yellow(RIGHT_EDGE,VIEW_HEIGHT-(i*SPRITE_HEIGHT),this,liam);
         m_vector.push_back(rightLine);
-    }/*
+    }
     for(int j = 0; j < M; j++)
     {
-        White* left = new White(left_middle,VIEW_HEIGHT-(j*SPRITE_HEIGHT + 6),this,liam);
+        White* left = new White(left_middle,VIEW_HEIGHT-((10*j)+SPRITE_HEIGHT),this,liam);
         m_vector.push_back(left);
-        White* right = new White(right_middle,VIEW_HEIGHT-(j*SPRITE_HEIGHT + 6),this,liam);
+        White* right = new White(right_middle,VIEW_HEIGHT-((10*j)+SPRITE_HEIGHT),this,liam);
         m_vector.push_back(right);
-    }*/
+    }
     /*
     Yellow* leftLine = new Yellow(LEFT_EDGE,VIEW_HEIGHT-(1*6),this,liam);
     m_vector.push_back(leftLine);
@@ -52,12 +52,15 @@ int StudentWorld::init()
 
 int StudentWorld::move()
 {
+    int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH/2;
+    int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH/2;
+    int N = VIEW_HEIGHT / SPRITE_HEIGHT;
+    int M = VIEW_HEIGHT / (4 * SPRITE_HEIGHT);
+    int left_middle = LEFT_EDGE + (ROAD_WIDTH / 3);
+    int right_middle = RIGHT_EDGE - (ROAD_WIDTH / 3);
     // This code is here merely to allow the game to build, run, and terminate after you hit enter.
     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-    vector<Actor*>::iterator it;
-    it = m_vector.begin();
-    m_vector.at(0)->doSomething();
-    for(int i = 1 ; i <= VIEW_HEIGHT / SPRITE_HEIGHT; i++)
+    for(int i = 0 ; i < m_vector.size(); i++)
     {
         m_vector.at(i)->doSomething();
         if(m_vector.at(i)->getY() < 0 && m_vector.at(i)->getX() < ROAD_CENTER)
@@ -66,8 +69,10 @@ int StudentWorld::move()
             m_vector.erase(m_vector.begin()+i);
             Yellow* leftLine = new Yellow(ROAD_CENTER - ROAD_WIDTH/2,VIEW_HEIGHT,this,temp);
             m_vector.push_back(leftLine);
-            ( m_vector.back() )->doSomething();
-            //delete m_vector.at(i);
+            //m_vector.back()->doSomething();
+            White* left = new White(left_middle, VIEW_HEIGHT,this,temp);
+            m_vector.push_back(left);
+            //m_vector.back()->doSomething();
         }
         if(m_vector.at(i)->getY() < 0 && m_vector.at(i)->getX() > ROAD_CENTER)
         {
@@ -75,8 +80,10 @@ int StudentWorld::move()
             m_vector.erase(m_vector.begin()+i);
             Yellow* rightLine = new Yellow(ROAD_CENTER + ROAD_WIDTH/2,VIEW_HEIGHT,this,temp);
             m_vector.push_back(rightLine);
-            m_vector.back()->doSomething();
-            //delete m_vector.at(i);
+            //m_vector.back()->doSomething();
+            White* right = new White(right_middle, VIEW_HEIGHT,this,temp);
+            m_vector.push_back(right);
+            //m_vector.back()->doSomething();
         }
     }
 
